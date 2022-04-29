31d3ecb9923dd24d423beb2da3a7d00d5d5f92c0
Kym Phillpotts <kphillpotts@gmail.com>
2022-04-29 21:05:34 +1000

    Remove boiler plate code

--- src/maui-dotnet-bot/MainPage.xaml.cs
+++ src/maui-dotnet-bot/MainPage.xaml.cs
@@ -2,19 +2,11 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
 
 	public MainPage()
 	{
 		InitializeComponent();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
-		CounterLabel.Text = $"Current count: {count}";
-
-		SemanticScreenReader.Announce(CounterLabel.Text);
-	}
 }
 
